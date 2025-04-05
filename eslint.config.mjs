af916b9f2faa09462d898572f8d1b1cce19ef360
Vladyslav <prostoandrei9@gmail.com>
2025-04-05 17:05:16 +0300

    aaaaa

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/ban-ts-comment": "off"
     }
   }
 ];
